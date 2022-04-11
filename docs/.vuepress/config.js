fa71a7daf3c3dc9cff44836a22832bdbd760aced
Sam Jones <sam@slashequip.com>
2022-04-11 20:20:07 +0000

    favicon et al

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,15 @@ module.exports = {
     lang: 'en-US',
     title: 'Laravel Pipeline',
     description: 'Documentation for the Laravel Pipeline package by SlashEquip.',
+    head: [
+        ['link', {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png'}],
+        ['link', {rel: 'icon', 'type': 'image/png', sizes: '32x32', href: '/favicon-32x32.png'}],
+        ['link', {rel: 'icon', 'type': 'image/png', sizes: '16x16', href: '/favicon-16x16.png'}],
+        ['link', {rel: 'manifest', href: '/site.webmanifest'}],
+        ['link', {rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'}],
+        ['meta', {name: 'msapplication-TileColor', content: '#ffffff'}],
+        ['meta', {name: 'theme-color', content: '#ffffff'}],
+    ],
 
     // theme and its config
     theme: '@vuepress/theme-default',
